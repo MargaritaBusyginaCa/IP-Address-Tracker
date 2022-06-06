f73c7380a6643a5efeabb284c500e29f1eae3734
MargaritaCa <busy0001@algonquinlive.com>
2022-06-05 17:10:00 -0700

    complete js and logic

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,13 @@ import '../index.css'
 //e5c74ddd755d4e99a32116947e8fd0fb
 function App (){
   const [infoIP, setInfoIP] = useState({})
-  const [IPAddress, setIPAddress] = useState("8.8.8.8")
+  const [IPAddress, setIPAddress] = useState("")
   const [latitude, setLatitude] = useState("32.58997")
   const [longitude, setLongitude] = useState("-92.06862")
   const [reload, setReload] = useState(false)
+  
   useEffect(() => {
+    map.remove()
      let ipObj = {}
      /**API Call */
      fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=e5c74ddd755d4e99a32116947e8fd0fb&ip=${IPAddress}`)
@@ -21,30 +23,42 @@ function App (){
                  latitude: ipObj.latitude, longitude: ipObj.longitude, time_zone_name: ipObj.time_zone.name,
                  time_zone_offset:ipObj.time_zone.offset})
        })
-     /**emd of API Call */
-     var map = L.map('map').setView([latitude, longitude], 11);
-     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-       maxZoom: 19,
-       attribution: '© OpenStreetMap'
-       }).addTo(map);
-     var marker = L.marker([51.5, -0.09]).addTo(map);
-
+     /**emd of API Call */   
   }, [reload])/**emd of useEffect */
 
   
-  function handleSubmit(e){
+  function handleOnChange(e){
+    map.remove()
     e.preventDefault()
-    setIPAddress(e.target.value)
-    console.log(IPAddress) 
+    setIPAddress(e.target.value) 
   }
+  function handleSubmit(){
+    setReload(prevReload => !prevReload)
+    console.log(reload)
+    map.remove()
+  }
+
+  var map = L.map('map').setView([latitude, longitude], 11);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+       maxZoom: 19,
+       attribution: '© OpenStreetMap'
+       }).addTo(map);
+   var marker = L.marker([latitude, longitude]).addTo(map);
+
   return(
       <div>
         <form>
           <input type="text" 
-                 
+                 value={IPAddress}
                  name="ip" 
-                 placeholder="IP Address"/>
+                 placeholder="IP Address"
+                 onChange={handleOnChange}/>
           <button type="button" onClick={handleSubmit}>Submit</button>
+          <p>{infoIP.country_name}</p>
+          <p>{infoIP.district}</p>
+          <p>{infoIP.time_zone_name}</p>
+          <p>{infoIP.latitude}</p>
+          <p>{infoIP.longitude}</p>
         </form>    
       </div>
   )
