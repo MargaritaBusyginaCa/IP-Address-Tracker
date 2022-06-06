7e7dbe3a0a500fda3f3016e49a187587bd357b87
MargaritaCa <busy0001@algonquinlive.com>
2022-06-06 11:53:24 -0700

    add styles for the header and form

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,9 @@ function App (){
   const [latitude, setLatitude] = useState("32.58997")
   const [longitude, setLongitude] = useState("-92.06862")
   const [reload, setReload] = useState(false)
-  
+ 
   useEffect(() => {
-    map.remove()
+     map.remove()
      let ipObj = {}
      /**API Call */
      fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=e5c74ddd755d4e99a32116947e8fd0fb&ip=${IPAddress}`)
@@ -24,7 +24,8 @@ function App (){
                  time_zone_offset:ipObj.time_zone.offset})
        })
      /**emd of API Call */   
-  }, [reload])/**emd of useEffect */
+    
+  }, [reload])/**end of useEffect */
 
   
   function handleOnChange(e){
@@ -33,33 +34,55 @@ function App (){
     setIPAddress(e.target.value) 
   }
   function handleSubmit(){
-    setReload(prevReload => !prevReload)
-    console.log(reload)
     map.remove()
+    setReload(prevReload => !prevReload)
+    
   }
 
   var map = L.map('map').setView([latitude, longitude], 11);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-       maxZoom: 19,
-       attribution: '© OpenStreetMap'
-       }).addTo(map);
-   var marker = L.marker([latitude, longitude]).addTo(map);
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+      }).addTo(map);
+  var marker = L.marker([latitude, longitude]).addTo(map);
+  
 
   return(
-      <div>
+      <div className="main--container">
+        <div className="header--container">
+          <h1>IP Address Tracker</h1>
         <form>
           <input type="text" 
                  value={IPAddress}
                  name="ip" 
-                 placeholder="IP Address"
+                 placeholder="Search for any IP address or domain"
                  onChange={handleOnChange}/>
-          <button type="button" onClick={handleSubmit}>Submit</button>
-          <p>{infoIP.country_name}</p>
-          <p>{infoIP.district}</p>
-          <p>{infoIP.time_zone_name}</p>
-          <p>{infoIP.latitude}</p>
-          <p>{infoIP.longitude}</p>
-        </form>    
+          <button type="button" onClick={handleSubmit} className="submit-btn"></button>
+        </form>
+       </div>
+        <div className="ip-info--container">
+
+         <div className="ip-address">
+          <p>IP ADDRESS</p>
+          <h3>{IPAddress}</h3>
+         </div>
+
+         <div>
+          <p>LOCATION</p>
+          <h3>{infoIP.district}</h3>
+         </div>
+
+         <div>
+          <p>TIMEZONE</p>
+          <h3>UTC {infoIP.time_zone_offset}:00</h3>
+         </div>
+
+         <div>
+          <p>COUNTRY</p>
+          <h3>{infoIP.country_name}</h3>
+         </div> 
+       </div> 
+
       </div>
   )
 }
